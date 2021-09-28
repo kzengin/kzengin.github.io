554aac068f59deb1eed9c658b4801ca01b11985e
Kerim Zengin <87282091+kzengin@users.noreply.github.com>
2021-09-28 15:52:10 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,4 +247,18 @@
 		$banner
 			._parallax();
 	
+	// Smooth Scrolling
+	$('a[href*="#"]:not([href="#navPanel"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+			$('html, body').animate({
+			  scrollTop: target.offset().top
+			}, 1000);
+			return false;
+		  }
+		}
+	  });
+	
 })(jQuery);
